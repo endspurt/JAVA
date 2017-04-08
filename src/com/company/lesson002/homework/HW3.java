a267008c7ec615576fe56228d299538f01cdc9b5
DenisMD <markovdenis2012@gmail.com>
2017-04-08 22:38:45 +0500

    Домашняя работа

---
+++ src/com/company/lesson002/homework/HW3.java
@@ -0,0 +1,8 @@
+package com.company.lesson002.homework;
+
+// Вещественные числа
+// https://ru.wikipedia.org/wiki/%D0%93%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D0%B0%D1%86%D0%B8%D1%8F
+// Запрограммировать рассчет гравитации
+// можно использовать массу Земли и Луны и растояние между ними
+public class HW3 {
+}
