7fc77f5339cd6407b0783deb2f5c58eec68b691d
Alex <az@teconisy.com>
2017-04-27 13:11:57 +0200

    sdelal

--- src/com/company/lesson002/homework/HW4.java
+++ src/com/company/lesson002/homework/HW4.java
@@ -2,4 +2,19 @@ package com.company.lesson002.homework;
 
 // Через цикл вывести все символы анг. алфавита, в нижнем регистре, начинается с кода 97 и заканчивается кодом 122
 public class HW4 {
-}
+
+        public static void main(String[] args) {
+            int a = 97;
+
+            while (a!=123){
+
+
+                System.out.print((char) a + ",");
+                a++;
+
+            };
+
+        }
+
+
+    }
